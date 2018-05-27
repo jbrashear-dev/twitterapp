bd27e221fec1238a3e21c2d90b2d3fbf86117eca
jbrashear-dev <33000782+jbrashear-dev@users.noreply.github.com>
2018-05-27 08:28:28 -0400

    Delete config.js

--- js/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    "consumer_key": "widli5qCoE333vo9ZSIfkRFUE"
-  , "consumer_secret": "Edg6U5L1a3j3Q9h12HZDBbKutge9HqqMhHMCO8zJkbfR8Zjpdg"
-  , "access_token": "96988509-3Y3gChnd8ocGwwd4cXNyHNsor2y9IOcPYoWg4w09u"
-  , "access_token_secret": "Jkbq137Fe5SudiGngFRyXBKirCRavxFRJ3OSNWWS3CUs2"
-}
